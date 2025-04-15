f6951f6a733f9110cfb15c2228ef6c7067b5a045
Luca Vieira <lucamevieira@gmail.com>
2025-04-15 16:02:55 -0300

    fix: morador migration

--- database/migrations/2025_04_15_162843_create_moradors_table.php
+++ database/migrations/2025_04_15_162843_create_moradors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('moradores', function (Blueprint $table) {
+        Schema::create('moradors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('moradores');
+        Schema::dropIfExists('moradors');
     }
 };
